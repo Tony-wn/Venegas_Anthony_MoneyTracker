93dd522a6eb238e58a8bf816cb715fdf6411c343
Tony-wn <anthonyvenegas540@gmail.com>
2025-12-03 17:49:48 -0500

    Entidad Transaction

--- app/src/main/java/com/venegas/moneytracker/data/local/entity/TransactionEntity.java
+++ app/src/main/java/com/venegas/moneytracker/data/local/entity/TransactionEntity.java
@@ -25,4 +25,68 @@ public class TransactionEntity {
         this.paymentMethod = paymentMethod;
         this.createdAt = System.currentTimeMillis();
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
 }
